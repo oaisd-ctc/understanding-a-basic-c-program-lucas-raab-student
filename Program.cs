c95c6d8f9591aa908d19ce02fea58a0fa1a8013f
lucas-raab-student <lurac3d@student.oaisd.org>
2024-10-09 14:31:11 +0000

    finished

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+        Console.WriteLine("my name is Lucas raab");
+
+        int age = 17;
+        string greeting = "Hello World";
+        string firstname = "Lucas";
+        string lastname = "raab";
+        double money = 2.79;
+
+        Console.WriteLine(greeting + "!" + "My name is" + firstname + " " + lastname + ". I am" + age + " years old. I just spent $" + money + " on an Energy Drink");
+
+    }
+}
